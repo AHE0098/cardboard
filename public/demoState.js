16701855f9759858a6166c18f123ba0769c17c12
AHE0098 <andreas_henriksen@live.dk>
2026-02-10 07:11:02 +0100

    Update demoState.js

--- public/demoState.js
+++ public/demoState.js
@@ -2,7 +2,8 @@ window.DEMO_STATE = {
   playerName: "Player 1",
   zones: {
     hand: [1, 2, 3, 4, 5, 6, 7],
-    lands: [],
-    permanents: []
-  }
+    lands: [21, 22, 23],
+    permanents: [31, 32]
+  },
+  tapped: {} // e.g. { "21": true }
 };
