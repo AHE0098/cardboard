44a050eaa1698ce388c40bb0e1823a9f41240c63
AHE0098 <andreas_henriksen@live.dk>
2026-02-10 09:21:51 +0100

    Create cards.js

---
+++ public/cards.js
@@ -0,0 +1,8 @@
+window.CARD_REPO = {
+  "1": { name: "Grizzly Bears", color: "green", power: 2, toughness: 2 },
+  "2": { name: "Shock", color: "red", image: "https://via.placeholder.com/200x280?text=Shock" },
+  "3": { name: "Island", color: "blue" },
+  "4": { name: "Goblin Raider", color: "red", power: 2, toughness: 2 },
+  "5": { name: "Plains", color: "white" },
+  // ...add more
+};
